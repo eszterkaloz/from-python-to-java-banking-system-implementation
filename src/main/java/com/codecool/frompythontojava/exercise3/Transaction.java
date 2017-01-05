337144a89e98dddba56444d23df3269b673691bd
eszterkaloz <eszterkaloz@gmail.com>
2017-01-05 12:03:54 +0100

    Implemented abstract model for Transaction

---
+++ src/main/java/com/codecool/frompythontojava/exercise3/Transaction.java
@@ -0,0 +1,28 @@
+package com.codecool.frompythontojava.exercise3;
+
+
+import java.util.Currency;
+import java.util.UUID;
+
+abstract class Transaction {
+    private UUID id;
+    private float amount;
+    private Currency currency;
+
+    Transaction(float amount, Currency currency) {
+        this.id = UUID.randomUUID();
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected abstract Receipt complete() throws Cancelled;
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", currency=" + currency +
+                '}';
+    }
+}
